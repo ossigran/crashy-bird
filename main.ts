33782009d787c27343acf6f410d1b5f996009079
ossigran <ossian.granasen@gmail.com>
2026-02-08 13:16:41 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     Bird.change(LedSpriteProperty.Y, -1)
-    music.play(music.createSoundExpression(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.setVolume(0)
 })
 input.onButtonPressed(Button.B, function () {
     Bird.change(LedSpriteProperty.Y, 1)
-    music.play(music.createSoundExpression(WaveShape.Square, 623, 403, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.setVolume(127)
 })
 let empty0bstacley = 0
 let Ticks = 0
 let Bird: game.LedSprite = null
 let score = 0
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground)
 let plats = 0
 let Obstacles: game.LedSprite[] = []
 Bird = game.createSprite(0, 2)
-Bird.set(LedSpriteProperty.Blink, 300)
+Bird.set(LedSpriteProperty.Blink, 200)
 basic.forever(function () {
     while (Obstacles.length > 0 && Obstacles[0].get(LedSpriteProperty.X) == 0) {
         Obstacles.removeAt(0).delete()
+        score += 1
     }
     for (let Obstacle of Obstacles) {
         Obstacle.change(LedSpriteProperty.X, -1)
@@ -31,11 +37,12 @@ basic.forever(function () {
     }
     for (let Obstacle of Obstacles) {
         if (Obstacle.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && Obstacle.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
+            music.stopMelody(MelodyStopOptions.Background)
             basic.clearScreen()
-            basic.showString("GAME OVER ")
-            basic.clearScreen()
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground)
+            basic.showString("" + (score))
         }
     }
     Ticks += 1
-    basic.pause(1000)
+    basic.pause(500)
 })
