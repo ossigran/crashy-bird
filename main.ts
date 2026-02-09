d6d2ac9eb38435cef697829347dfe58dbb83ee1c
ossigran <ossian.granasen@gmail.com>
2026-02-09 15:09:43 +0100

    sen ska jag öka farten

--- main.ts
+++ main.ts
@@ -14,33 +14,37 @@ let empty0bstacley = 0
 let Ticks = 0
 let Bird: game.LedSprite = null
 let score = 0
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground)
 let plats = 0
 let Obstacles: game.LedSprite[] = []
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground)
 Bird = game.createSprite(0, 2)
 Bird.set(LedSpriteProperty.Blink, 200)
 basic.forever(function () {
     while (Obstacles.length > 0 && Obstacles[0].get(LedSpriteProperty.X) == 0) {
         Obstacles.removeAt(0).delete()
-        score += 1
+        score += 0.25
     }
     for (let Obstacle of Obstacles) {
         Obstacle.change(LedSpriteProperty.X, -1)
     }
     if (Ticks % 3 == 0) {
         empty0bstacley = randint(0, 4)
-        for (let plats = 0; plats <= 4; plats++) {
-            if (plats != empty0bstacley) {
-                Obstacles.push(game.createSprite(4, plats))
+        for (let plats2 = 0; plats2 <= 4; plats2++) {
+            if (plats2 != empty0bstacley) {
+                Obstacles.push(game.createSprite(4, plats2))
             }
         }
     }
-    for (let Obstacle of Obstacles) {
-        if (Obstacle.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && Obstacle.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
+    for (let Obstacle2 of Obstacles) {
+        if (Obstacle2.get(LedSpriteProperty.X) == Bird.get(LedSpriteProperty.X) && Obstacle2.get(LedSpriteProperty.Y) == Bird.get(LedSpriteProperty.Y)) {
             music.stopMelody(MelodyStopOptions.Background)
-            basic.clearScreen()
             music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground)
-            basic.showString("" + (score))
+            basic.clearScreen()
+            for (let index = 0; index < 100000000; index++) {
+                basic.showString("GAME OVER SCORE " + score)
+                basic.pause(5000)
+            }
+            score = 0
         }
     }
     Ticks += 1
