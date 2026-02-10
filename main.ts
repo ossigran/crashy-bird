5656fe46fbad59151e5f2635589c0a01fcc22abb
ossigran <ossian.granasen@gmail.com>
2026-02-10 19:26:26 +0100

    crasy bird utan ljud

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     Bird.change(LedSpriteProperty.Y, -1)
 })
+input.onGesture(Gesture.Shake, function () {
+	
+})
 input.onButtonPressed(Button.AB, function () {
     music.setVolume(0)
 })
 input.onButtonPressed(Button.B, function () {
     Bird.change(LedSpriteProperty.Y, 1)
-})
-input.onGesture(Gesture.Shake, function () {
-    music.setVolume(127)
 })
 let empty0bstacley = 0
 let Ticks = 0
@@ -20,6 +20,7 @@ music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.
 Bird = game.createSprite(0, 2)
 Bird.set(LedSpriteProperty.Blink, 200)
 basic.forever(function () {
+    music.setVolume(0)
     while (Obstacles.length > 0 && Obstacles[0].get(LedSpriteProperty.X) == 0) {
         Obstacles.removeAt(0).delete()
         score += 0.25
@@ -40,7 +41,7 @@ basic.forever(function () {
             music.stopMelody(MelodyStopOptions.Background)
             music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground)
             basic.clearScreen()
-            for (let index = 0; index < 100000000; index++) {
+            for (let index = 0; index < 1e+110; index++) {
                 basic.showString("GAME OVER SCORE " + score)
                 basic.pause(5000)
             }
